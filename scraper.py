512d5b7086dad642fca91bff2d3c91a861af93af
Prasanna Jagathesan <prasanna.jagathesan@avalara.com>
2021-05-31 13:08:59 +0530

    Add pyppeteer support for js enabled pages

--- scraper.py
+++ scraper.py
@@ -3,10 +3,21 @@ from bs4 import BeautifulSoup, Tag
 from collections import Counter
 import sys
 import csv
+import asyncio
+from pyppeteer import launch
 
 
-def cssPath(element):
-    if not(isinstance(element, Tag)):
+async def main(request_url):
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+    await page.goto(request_url)
+    content = await page.content()
+    await browser.close()
+    return content
+
+
+def css_path(element):
+    if not (isinstance(element, Tag)):
         return
     path = []
     while isinstance(element, Tag):
@@ -24,27 +35,37 @@ def cssPath(element):
             nth = 1
             while sibling == sibling.previous:
                 if sibling.name.lower() == selector:
-                   nth += 1
+                    nth += 1
             if nth != 1:
-                selector += ":nth-of-type("+str(nth)+")"
+                selector += ":nth-of-type(" + str(nth) + ")"
         path = [selector] + path
         element = element.parent
     return " > ".join(path)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Not enough arguments! Please provide an input file.")
         sys.exit(1)
+    # Input file: TSV file with two fields (url, jsRenderRequired <accepted_values>: 'true', 'false')
+    # Please refer `sample.tsv` for better understanding
     input_file = str(sys.argv[1])
     tag = str(sys.argv[2])
     with open(input_file, 'r') as f:
-        urls = f.readlines()
+        input_rows = list(map(lambda line: line.split('\t'), f.readlines()))
     output = []
-    for url in urls:
+    for url, jsEnabled, *_ in input_rows:
         r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html5lib')
+        htmlContent = bytes()
+
+        if jsEnabled == "true":
+            htmlContent = bytes(asyncio.get_event_loop().run_until_complete(main(url)), encoding='UTF-8')
+        else:
+            htmlContent = r.content
+
+        soup = BeautifulSoup(htmlContent, 'html5lib')
         links = soup.findAll(tag)
-        paths = [cssPath(element) for element in links]
+        paths = [css_path(element) for element in links]
 
         for selector, count in Counter(paths).items():
             output.append([url, selector, count])
@@ -52,4 +73,4 @@ if __name__ == "__main__":
     with open('output.tsv', 'wt') as output_file:
         tsv_writer = csv.writer(output_file, delimiter='\t')
         tsv_writer.writerow(['url', 'selector', 'count'])
-        [ tsv_writer.writerow(row) for row in output ]
+        [tsv_writer.writerow(row) for row in output]
